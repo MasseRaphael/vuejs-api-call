0f8a532254a39875f365944f169b74f721fdd9a7
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2021-03-17 15:23:31 +0100

    appelle de l'api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,29 @@
 <template>
     <Layout>
-        <h1>Calendar API</h1>
+        <pre>{{ agendaResult }}</pre>
+        <button v-on:click="apiCall()">Afficher l'agenda</button>
     </Layout>
 </template>
+<script>
+export default{
+    data(){
+        return {
+            AGENDA_URL: "openagenda.com",
+            AGENDA_UID: '84339284',
+            AGENDA_KEY: 'f510f08cc7ab41feb394928a2123c677',
+            agendaResult: ''
+        }
+    },
+    methods: {
+        apiCall(){
+            fetch(`https://${this.AGENDA_URL}/agendas/${this.AGENDA_UID}/events.v2.json?key=${this.AGENDA_KEY}`)
+            .then(response =>{
+                return response.json()
+            })
+            .then(json => {
+                this.agendaResult = json
+            })
+        }
+    }
+}
+</script>
